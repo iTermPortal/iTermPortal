011a942f62c2776e8f63e0835825b79a9dbf9d81
hjoncour <hugo.joncour.k2o@gmail.com>
2025-10-14 21:47:36 -0400

    first-commit

---
+++ fPortal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fPortal
+//
+//  Created by Hugo Joncour on 2025-10-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fPortal/fPortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  fPortalApp.swift
+//  fPortal
+//
+//  Created by Hugo Joncour on 2025-10-14.
+//
+
+import SwiftUI
+
+@main
+struct fPortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
